78bfbb1c941380531bbc9273f4d3be9ac29b620f
Anderson Kennedy <andersonkennedy@MacBook-Air-de-Anderson.local>
2020-06-10 13:21:05 +0100

    config do express e add nodemon

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
+//uso do express para criar e configurar o servidor
 const express = require("express")
 const server = express()
-//console.log(server)
-//console.log(express)
-//"start": "nodemon server.js"
 
-//Configurar arquivos estaticos (css,scripts,imagens)
-server.use(express.static("public"))
-server.get("/",function(req,res){
-    return res.send("resposta do servidor")
+//criando rotas
+server.get("/",function(req, res){
+    return res.send("Resposta do server !")
 })
-
 server.listen(3000)
-
-console.log('Feito')
