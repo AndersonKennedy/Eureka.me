91c65345f3669b276aa1c9f3192cf9e9c0a9bbf1
Anderson Kennedy <andersonkennedy@MacBook-Air-de-Anderson.local>
2020-06-11 07:24:25 +0100

    move ideias file e config routes

--- server.js
+++ server.js
@@ -10,5 +10,9 @@ server.get("/",function(req, res){
     return res.sendFile(__dirname + "/index.html")
 })
 
+server.get("/ideias",function(req, res){
+    return res.sendFile(__dirname + "/ideias.html")
+})
+
 //ligar o servidor na porta 3000
 server.listen(3000)
