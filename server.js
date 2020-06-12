0822b7c18ee61e055ded93019593a9fd953b1192
Anderson Kennedy <andersonkennedy@MacBook-Air-de-Anderson.local>
2020-06-12 08:16:36 +0100

    config and testing nunjack

--- server.js
+++ server.js
@@ -5,13 +5,22 @@ const server = express()
 //Configurar arquivos estaticos
 server.use(express.static("public"))
 
+//configuração do Nunjacks: pacote que nos permite usar variavel no HTML
+const nunjucks = require("nunjucks")
+nunjucks.configure("views",{
+    express: server,
+    noCache: true,
+})
+
+
 //criando rotas
 server.get("/",function(req, res){
-    return res.sendFile(__dirname + "/index.html")
+    const h1 = "Olá do Backend"
+    return res.render("index.html",{ title: h1 })
 })
 
 server.get("/ideias",function(req, res){
-    return res.sendFile(__dirname + "/ideias.html")
+    return res.render("ideias.html")
 })
 
 //ligar o servidor na porta 3000
